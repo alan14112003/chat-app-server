fa6793eab1d2e1c9de9d66f061bac81f018012b7
alan.nns <alan.nns.14.11@gmail.com>
2024-10-15 08:48:32 +0700

    update: add timestamp fied in chat model

--- src/app/models/Chat.model.js
+++ src/app/models/Chat.model.js
@@ -1,31 +1,25 @@
 import { DataTypes, UUID, UUIDV4 } from 'sequelize'
 import SequelizeConfig from '@/config/Sequelize.config'
 
-const Chat = SequelizeConfig.define(
-  'Chat',
-  {
-    id: {
-      type: UUID,
-      defaultValue: UUIDV4,
-      allowNull: false,
-      primaryKey: true,
-    },
-    groupName: {
-      type: DataTypes.STRING,
-    },
-    lastMessage: {
-      type: DataTypes.BIGINT,
-    },
-    groupAdmin: {
-      type: DataTypes.UUID,
-    },
-    isGroup: {
-      type: DataTypes.BOOLEAN,
-    },
+const Chat = SequelizeConfig.define('Chat', {
+  id: {
+    type: UUID,
+    defaultValue: UUIDV4,
+    allowNull: false,
+    primaryKey: true,
   },
-  {
-    timestamps: false,
-  }
-)
+  groupName: {
+    type: DataTypes.STRING,
+  },
+  lastMessage: {
+    type: DataTypes.BIGINT,
+  },
+  groupAdmin: {
+    type: DataTypes.UUID,
+  },
+  isGroup: {
+    type: DataTypes.BOOLEAN,
+  },
+})
 
 export default Chat
